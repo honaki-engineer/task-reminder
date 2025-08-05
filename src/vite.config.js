9524d4f06c9e53187d6b43ea9929d215a5d1b566
HondaAkihito <honaki.and.1114@gmail.com>
2025-08-05 18:37:15 +0900

    first commit

---
+++ src/vite.config.js
@@ -0,0 +1,35 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
