3e0d117114ac3b61ffe0ae2e69ffacae5150ff38
HondaAkihito <honaki.and.1114@gmail.com>
2025-08-06 23:01:33 +0900

    feat: モデル/マイグレーションの作成

---
+++ src/database/migrations/2025_08_05_194106_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('task_category_id')->constrained()->nullable();
+            $table->string('title', 50);
+            $table->text('description')->nullable();
+            $table->date('start_at');
+            $table->date('end_at');
+            $table->boolean('is_completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
