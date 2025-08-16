b689ce68208b5a0212cc817e0cab0611b8f7303b
HondaAkihito <honaki.and.1114@gmail.com>
2025-08-16 23:13:23 +0900

    feat: エラー画面作成

---
+++ src/resources/views/components/errors/error-page.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $code }} | {{ $title }}</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 text-gray-800 flex flex-col min-h-screen overflow-y-scroll">
+
+    {{-- ヘッダー --}}
+    <header class="bg-gray-200 border-b border-gray-100">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="flex justify-between h-16">
+              <div class="flex">
+                  <!-- Logo -->
+                  <div class="shrink-0 flex items-center">
+                      <a href="{{ route('tasks.one_day') }}">
+                          <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </header>
+
+    {{-- メイン --}}
+    <main class="flex-grow flex flex-col justify-center items-center text-center px-4">
+        <h2 class="text-xl font-bold mb-2">{{ $code }} | {{ $title }}</h2>
+        <p class="mb-6">{!! $message !!}</p>
+        <button type="submit"
+            class="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 transition text-lg">
+                <a href="{{ route('tasks.one_day') }}">トップページはこちら</a>
+        </button>
+    </main>
+
+    {{-- フッター --}}
+    <footer class="shadow bg-gray-200 py-4 text-center text-sm text-gray-500">
+        © {{ date('Y') }} {{ config('app.name') }} All rights reserved
+    </footer>
+
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,16 +16,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ----- デフォルト使用しない
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
+// ----- エラー画面の確認テスト
+// 400 Bad Request
+Route::get('/force-400', function () { abort(400, 'Bad Request Test'); });
+// 401 Unauthorized
+Route::get('/force-401', function () { abort(401, 'Unauthorized Test'); });
+// 403 Forbidden
+Route::get('/force-403', function () { abort(403, 'Forbidden Test'); });
+// 404 Not Found
+Route::get('/force-404', function () { abort(404, 'Not Found Test'); });
+// 419 Page Expired
+Route::get('/force-419', function () { abort(419, 'Page Expired Test'); });
+// 422 Unprocessable Entity
+Route::get('/force-422', function () { abort(422, 'Unprocessable Entity Test'); });
+// 429 Too Many Requests
+Route::get('/force-429', function () { abort(429, 'Too Many Requests Test'); });
+// 500 Internal Server Error（throwで例外を発生させる）
+Route::get('/force-500', function () { throw new \Exception('Internal Server Error Test'); });
+// 503 Service Unavailable
+Route::get('/force-503', function () { abort(503, 'Service Unavailable Test'); });
+
+// ----- ゲストログイン
 Route::get('/guest-login', [GuestLoginController::class, 'login'])->name('guest.login');
 
+// ----- ログイン中のみ
 Route::middleware('auth')->group(function () {
     Route::get('/tasks/one-day', [TaskController::class, 'oneDay'])->name('tasks.one_day');
     Route::post('/tasks/{task}', [TaskController::class, 'complete'])->name('tasks.complete');
