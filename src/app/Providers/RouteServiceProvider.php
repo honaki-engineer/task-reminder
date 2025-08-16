3ee3ce5fecb387b9e5c47c7ff2e769fff5927b55
HondaAkihito <honaki.and.1114@gmail.com>
2025-08-16 13:46:20 +0900

    fix: ログイン/ログアウト後の遷移先を修正

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/tasks/one-day';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
