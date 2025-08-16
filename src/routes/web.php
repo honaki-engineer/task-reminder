de9649d9fbd772cf8401d5cf52783208ccf4b8e1
HondaAkihito <honaki.and.1114@gmail.com>
2025-08-16 13:50:19 +0900

    fix: welcomeページとdashboardページを非表示に変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/tasks/one-day', [TaskController::class, 'oneDay'])->name('tasks.one_day');
